788c2c54b593213bc23fb97a7ad2f0932935b656
Kaare Rasmussen <kaare@jasonic.dk>
2011-01-02 19:30:25 +0100

    Spaces to tabs

--- lib/Kontor/Gl.pm
+++ lib/Kontor/Gl.pm
@@ -10,28 +10,28 @@ use Kontor::Form::Gl::Daybook;
 use base 'Mojolicious::Controller';
 
 sub index {
-    my $self = shift;
-    $self->render_text('Welcome to the World of Accounting!');
+	my $self = shift;
+	$self->render_text('Welcome to the World of Accounting!');
 }
 
 sub daybook {
-    my $self = shift;
-    my $model = $self->model();
-    my $soas = $model->resultset('Gl::Accountsoa')->search;
+	my $self = shift;
+	my $model = $self->model();
+	my $soas = $model->resultset('Gl::Accountsoa')->search;
 	my @banks;
-    while (my $soa = $soas->next) {
-    	push @banks, {
+	while (my $soa = $soas->next) {
+		push @banks, {
 			acctname => $soa->coa->name,
 			balance => $soa->balance,
 		}
-    }
-    my $batch;
-    # my $batch = $model->resultset('Gl::Batch')->create({
-    	# org_id => 4,
+	}
+	my $batch;
+	# my $batch = $model->resultset('Gl::Batch')->create({
+		# org_id => 4,
 		# batchnr => 234,
 		# postingdate => DateTime->now,
-    # });
-    my $lines = [
+	# });
+	my $lines = [
 		{
 			date => '2010-12-24',
 			journalnr => 1,
@@ -40,7 +40,7 @@ sub daybook {
 			amount => 1.25,
 			banks => [(0) x @banks]
 		}
-    ];
+	];
 	my $form = Kontor::Form::Gl::Daybook->new;
 	my $params = {
 		'difference.0.value' => 1.50,
@@ -48,7 +48,7 @@ sub daybook {
 		'difference.3.value' => 3.50,
 	};
 	$form->process( params => $params );
-    $self->render(batch => $batch, banks => \@banks, lines => $lines, form => $form);
+	$self->render(batch => $batch, banks => \@banks, lines => $lines, form => $form);
 }
 
 1;
