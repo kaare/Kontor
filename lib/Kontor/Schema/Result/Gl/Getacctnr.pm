eb86fd4f54505dc43f12b3324315ac26abb42a28
Kaare Rasmussen <karm@novozymes.com>
2013-09-24 11:07:30 +0200

    Update "view" classes to use current DBIC implementation

--- lib/Kontor/Schema/Result/Gl/Getacctnr.pm
+++ lib/Kontor/Schema/Result/Gl/Getacctnr.pm
@@ -1,12 +1,17 @@
 package Kontor::Schema::Result::Gl::Getacctnr;
+
 use strict;
 use warnings;
-use base qw/DBIx::Class/;
+use base qw/DBIx::Class::Core/;
 
-__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
 __PACKAGE__->table( "dummy" );
 __PACKAGE__->add_columns(qw/acctnr/);
 
-__PACKAGE__->result_source_instance->name(\'(SELECT get_acctnr AS acctnr FROM gl.get_acctnr(?, ?))');
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+__PACKAGE__->result_source_instance->view_definition(q{
+    SELECT get_acctnr AS acctnr FROM gl.get_acctnr(?, ?)
+});
 
 1;
--- lib/Kontor/Schema/Result/Gl/Getdimensions.pm
+++ lib/Kontor/Schema/Result/Gl/Getdimensions.pm
@@ -1,12 +1,17 @@
 package Kontor::Schema::Result::Gl::Getdimensions;
+
 use strict;
 use warnings;
-use base qw/DBIx::Class/;
+use base qw/DBIx::Class::Core/;
 
-__PACKAGE__->load_components(qw/Core/);
+__PACKAGE__->table_class('DBIx::Class::ResultSource::View');
 __PACKAGE__->table( "dummy" );
 __PACKAGE__->add_columns(qw/dims/);
 
-__PACKAGE__->result_source_instance->name(\'(SELECT get_dimensions AS dims FROM gl.get_dimensions(?, ?))');
+__PACKAGE__->result_source_instance->is_virtual(1);
+
+__PACKAGE__->result_source_instance->view_definition(q{
+    SELECT get_dimensions AS dims FROM gl.get_dimensions(?, ?)
+});
 
 1;
